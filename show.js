6bac02e19f15e92b4b57113fbe2b4ba7598f9859
willhu85 <willhu85@gmail.com>
2019-07-10 23:05:30 +0800

    更新CSS文件目录

--- show.js
+++
@@ -1,5 +0,0 @@
-function show(content) {
-  window.document.getElementById("app").innerText = "hello！" + content;
-}
-
-module.exports = show;
--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
   // JavaScript 执行入口文件
-  entry: "./index.js",
+  entry: "./index.ts",
   // entry: { a: "./index.js", b: "./index2.js" },
   output: {
     // 把所有依赖的模块合并输出到一个 bundle.js 文件
@@ -23,17 +23,23 @@ module.exports = {
         test: /\.scss$/,
         use: ["style-loader", "css-loader", "scss-loader"],
         include: path.resolve(__dirname, "src")
+      },
+      {
+        test: /\.ts$/,
+        loader: "awesome-typescript-loader"
       }
     ]
   },
   resolve: {
     alias: {
       show: "./show.js"
-    }
+    },
+    extensions: [".ts", ".js"]
   },
   plugins: [
     new ExtractTextPlugin({
       filename: `[name]_[contenthash:8].css`
     })
-  ]
+  ],
+  devtool: "source-map"
 };
